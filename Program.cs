a3883b6fa8fb427f973acc5c751651909adf2577
IT22362476 <denuwansathsara0412@gmail.com>
2026-05-29 12:36:30 +0530

    Enable Swagger in all environments

--- Program.cs
+++ Program.cs
@@ -105,11 +105,13 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
+// Enable Swagger in all environments (useful for Azure deployment)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Manager API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseCors("AllowAngular");
 app.UseAuthentication();
